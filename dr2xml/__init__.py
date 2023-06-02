b9d406d5cf7bb5103f3afc7ee8ed793827d1a966
Gaëlle Rigoudy <gaelle.rigoudy@meteo.fr>
2023-06-02 12:40:15 +0200

    TODO: license

--- dr2xml/__init__.py
+++ dr2xml/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Copyright or © or Copr. CNRM (Météo-France, CNRS), Cerfacs/CECI (CNRS) (2016)
+
 In the context of Climate Model Intercomparison Projects (CMIP) :
 
 A few functions for processing
@@ -28,6 +30,34 @@ Changes :
                  Handle remapping for CFsites
 
 Rather look at git log for identifying further changes and contributors....
+
+Contact point: dr2xml_dev@meteo.fr
+
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info".
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-C license and that you accept its terms.
 
 """
 ####################################
