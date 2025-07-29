437d8f712f5d0e68818787fd9093a3324f0c4f75
rigoudyg <gaelle.rigoudy@meteo.fr>
2025-07-29 11:50:26 +0200

    Update pingfiles tests to v1.2.2 of CMIP7 DR

--- tests/test_AOESM_historical_CMIP7/input/lab_and_model_settings.py
+++ tests/test_AOESM_historical_CMIP7/input/lab_and_model_settings.py
@@ -4,8 +4,7 @@
 
 lab_and_model_settings = {
     'data_request_used': 'CMIP7',
-    'data_request_path': '/home/rigoudyg/dev/data_request/CMIP7_DReq_Software/data_request_api',
-    'data_request_content_version': 'v1.2.1',
+    'data_request_content_version': 'v1.2.2',
     'project': 'CMIP7',
     'path_to_parse': '{path_xml}/',
     'comment': '',
