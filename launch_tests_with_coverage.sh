1333af5f678bb6359510ca3b042b5670e8ebbdb4
rigoudyg <gaelle.rigoudy@meteo.fr>
2025-10-16 11:41:05 +0200

    Update tests with new version of the DR software

--- launch_tests_with_coverage.sh
+++ launch_tests_with_coverage.sh
@@ -5,14 +5,18 @@ set -e
 
 coverage erase
 
-coverage run
+# coverage run
+for f in $(ls tests/test_*.py); do
+    coverage run --parallel-mode -m unittest $f
+done
+for f in $(ls tests/test_*/__init__.py); do
+    coverage run --parallel-mode -m unittest $f
+done
 coverage run --parallel-mode scripts/create_docs_projects.py --target_directory=test
 coverage run --parallel-mode scripts/check_outputs_produced.py --out=test tests/xml_outputs/dr2xml_trip.xml --ioxdir=tests/xml_outputs/outputs --fatal=0
 coverage run --parallel-mode scripts/check_outputs_produced.py --out=test tests/xml_outputs/dr2xml_trip.xml --ioxdir=tests/xml_outputs/outputs2
 coverage run --parallel-mode scripts/find_netcdf_names_from_xml_files.py --out=test tests/xml_outputs/dr2xml_trip.xml
 coverage run --parallel-mode scripts/create_ping_files.py --lab=cnrm --out=test
-# coverage run --parallel-mode scripts/create_ping_files.py --lab=cerfacs --out=test
-# coverage run --parallel-mode scripts/create_ping_files.py --lab=ipsl --out=test
 coverage combine
 
 coverage html
